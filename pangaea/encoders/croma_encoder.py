6f04eb4d1b3250286680e220b6df612f79885139
Giovanni Castiglioni <gcastiglioni@kraken.ialab>
2025-02-21 20:12:21 -0300

    Added from_scratch argument to weights loaders

--- pangaea/encoders/croma_encoder.py
+++ pangaea/encoders/croma_encoder.py
@@ -107,24 +107,26 @@ class CROMA_OPTICAL_Encoder(Encoder):
 
         return output
 
-    def load_encoder_weights(self, logger: Logger) -> None:
-        pretrained_model = torch.load(self.encoder_weights, map_location="cpu", weights_only=False)[
-            "s2_encoder"
-        ]
-        k = pretrained_model.keys()
-        pretrained_encoder = {}
-        incompatible_shape = {}
-        missing = {}
-        for name, param in self.s2_encoder.named_parameters():
-            if name not in k:
-                missing[name] = param.shape
-            elif pretrained_model[name].shape != param.shape:
-                incompatible_shape[name] = (param.shape, pretrained_model[name].shape)
-            else:
-                pretrained_encoder[name] = pretrained_model[name]
+    def load_encoder_weights(self, logger: Logger, from_scratch: bool = False) -> None:
+        if not from_scratch:
+            pretrained_model = torch.load(self.encoder_weights, map_location="cpu", weights_only=False)[
+                "s2_encoder"
+            ]
+            k = pretrained_model.keys()
+            pretrained_encoder = {}
+            incompatible_shape = {}
+            missing = {}
+            for name, param in self.s2_encoder.named_parameters():
+                if name not in k:
+                    missing[name] = param.shape
+                elif pretrained_model[name].shape != param.shape:
+                    incompatible_shape[name] = (param.shape, pretrained_model[name].shape)
+                else:
+                    pretrained_encoder[name] = pretrained_model[name]
 
-        self.s2_encoder.load_state_dict(pretrained_encoder, strict=False)
-        self.parameters_warning(missing, incompatible_shape, logger)
+            self.s2_encoder.load_state_dict(pretrained_encoder, strict=False)
+            self.parameters_warning(missing, incompatible_shape, logger)
+        else:pass
 
 
 class CROMA_SAR_Encoder(Encoder):
